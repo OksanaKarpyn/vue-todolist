00cc96dde2be5af99aac0bb94d7a9378e9bb46a7
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-04-20 16:35:11 +0200

    al click di x viene eliminato elemento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,56 @@
-const { createApp } = Vue
+const {
+  createApp
+} = Vue
 
-  createApp({
-    data() {
-      return {
-        currentActive: 0,
-        control:false,
-        classCss:'input:checked + label',
-        class:'bg-info',
-        valueInput:'',
-        todos: [
-          {
-            text: 'Fare i compiti',
-            done: false
-          },
-          {
-            text: 'Fare la spesa',
-            done: true
-          },
-          {
-            text: 'Fare il bucato',
-            done: false
-          }
-        ]
-     
+createApp({
+  data() {
+    return {
+      control: false,
+      currentActive: 0,
+      class: 'bg-info',
+      valueInput: '',
+      todos: [{
+          text: 'Fare i compiti',
+          done: false
+        },
+        {
+          text: 'Fare la spesa',
+          done: true
+        },
+        {
+          text: 'Fare il bucato',
+          done: false
+        }
+      ]
+
+    }
+  },
+  methods: {
+    newtodo() {
+      if(this.valueInput !== ''){
+        let oggettoInput = {
+          text:this.valueInput,
+          done:false }
+        this.todos.push(oggettoInput)
+          // pulisce campo input
+          this.valueInput = ''
       }
     },
-    methods: {
-      
-      newtodo() {
-        this.todos.push(this.valueInput),
-        this.valueInput=''
+    todoTrue() {
+      if (this.todos == true) {
+        this.todos = this.class
+        // this.control = false
+      } else {
+        // this.control = true
       }
-      
-    }
-    
-  }).mount('#app')
+    },
+
+    deleteItem(i) {
+      console.log("delete",i);
+      // i sta per posixione del elemento, 1 quantita di elementi da eliminare
+      this.todos.splice(i, 1);
+    },
+
+  }
+
+}).mount('#app')
